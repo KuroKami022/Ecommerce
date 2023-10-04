7356d7d867ec8fed1a4ca24c299e87272fb54336
Kami <luisru693@gmail.com>
2023-10-04 11:07:42 -0400

    product-details.open/close/styles

--- Ecommerce/src/Context/index.jsx
+++ Ecommerce/src/Context/index.jsx
@@ -3,13 +3,26 @@ import { createContext, useState } from "react";
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children}) => {
+    // S-C increase quantity logic
     const [count, setCount] = useState(0)
-    console.log('COUNT: ', count)
+    
+    // Product open-close
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
+
+    // Product detail show product
+    const [productToShow, setProductToShow] = useState({})
 
     return(
         <ShoppingCartContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>
