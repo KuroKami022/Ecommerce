5a211a619252cc03585078a77010396f2dac75e4
Kami <luisru693@gmail.com>
2023-10-04 11:14:35 -0400

    product-details.open/close/styles1.0

--- Ecommerce/src/Context/index.jsx
+++ Ecommerce/src/Context/index.jsx
@@ -1,30 +1,35 @@
-import { createContext, useState } from "react";
+import { createContext, useState } from 'react'
 
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children}) => {
-    // S-C increase quantity logic
-    const [count, setCount] = useState(0)
-    
-    // Product open-close
-    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
-    const openProductDetail = () => setIsProductDetailOpen(true)
-    const closeProductDetail = () => setIsProductDetailOpen(false)
+  // Shopping Cart · Increment quantity
+  const [count, setCount] = useState(0)
 
-    // Product detail show product
-    const [productToShow, setProductToShow] = useState({})
+  // Product Detail · Open/Close
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
+  const openProductDetail = () => setIsProductDetailOpen(true)
+  const closeProductDetail = () => setIsProductDetailOpen(false)
 
-    return(
-        <ShoppingCartContext.Provider value={{
-            count,
-            setCount,
-            openProductDetail,
-            closeProductDetail,
-            isProductDetailOpen,
-            productToShow,
-            setProductToShow
-        }}>
-            {children}
-        </ShoppingCartContext.Provider>
-    )
+  // Product Detail · Show product
+  const [productToShow, setProductToShow] = useState({})
+
+  // Shopping Cart · Add products to cart
+  const [cartProducts, setCartProducts] = useState([])
+
+  return (
+    <ShoppingCartContext.Provider value={{
+      count,
+      setCount,
+      openProductDetail,
+      closeProductDetail,
+      isProductDetailOpen,
+      productToShow,
+      setProductToShow,
+      cartProducts,
+      setCartProducts
+    }}>
+      {children}
+    </ShoppingCartContext.Provider>
+  )
 }
